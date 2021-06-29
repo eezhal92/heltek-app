8ff26df064d42e3c4eaf8b0fb728afbdeba91561
eezhal92 <arkinthesky.69@gmail.com>
2021-06-29 20:43:51 +0800

    add swagger docs

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,14 +1,21 @@
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import { errorHandler } from './middlewares/error';
 import router from './routes';
 
 const app = express();
+const swaggerDocs = YAML.load(path.resolve(__dirname, '../../docs/swagger/swagger.yaml'));
 
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 app.use(router);
 app.use(errorHandler);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
+  explorer: true,
+}))
 
 export default app;
